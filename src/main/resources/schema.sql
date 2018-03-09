a3a0aff4f29ea77220220b65d1206dbffe3c4158
Lezenford <plekhanov_a.v@mail.ru>
2018-03-09 22:28:44 +0300

    test telegram

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,7 @@
 DELETE FROM xmpp_accounts;
+DELETE FROM telegram_users;
 
-INSERT INTO xmpp_accounts (login, password, server, savehistory) VALUES ('testjava', '12345678', 'ifox.pro',1);
+
+INSERT INTO telegram_users (id, username) VALUES (1,'123');
+INSERT INTO xmpp_accounts (login, password, server, savehistory, telegramuser) VALUES ('testjava', '12345678', 'ifox.pro',1,1);
 # INSERT INTO xmpp_accounts (login, password, server) VALUES ('testjava2', '12345678', 'ifox.pro');

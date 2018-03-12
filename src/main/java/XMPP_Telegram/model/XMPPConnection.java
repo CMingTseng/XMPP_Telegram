4a3d501c2e64a0e7fec4b0b6ff5ea67a280d1507
Lezenford <plekhanov_a.v@mail.ru>
2018-03-12 13:00:27 +0300

    bugfix transfer from xmpp to telegram

--- src/main/java/XMPP_Telegram/model/XMPPConnection.java
+++ src/main/java/XMPP_Telegram/model/XMPPConnection.java
@@ -85,7 +85,7 @@ public class XMPPConnection {
                 public void newIncomingMessage(EntityBareJid from, Message message, Chat chat) {
                     if (message.getType().equals(Message.Type.chat) && message.getBody() != null) {
                         try {
-                            controller.receivedMessage(BotUtil.getXMPPServer(message.getTo().toString()), BotUtil.getXMPPLogin(message.getTo().toString()), BotUtil.getXMPPLogin(message.getFrom().toString()), message.getBody());
+                            controller.receivedMessage(server, login, BotUtil.getXMPPLogin(message.getFrom().toString()), message.getBody());
                             IQ iq = new EmptyResultIQ();
                             iq.setType(IQ.Type.result);
                             iq.setStanzaId(message.getStanzaId());

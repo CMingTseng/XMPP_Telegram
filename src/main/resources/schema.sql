a6286facf44231210008c3869ba9043f964e594d
Lezenford <plekhanov_a.v@mail.ru>
2018-03-18 16:10:06 +0300

    add commands

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -81,7 +81,7 @@ CREATE UNIQUE INDEX telegram_chats_index
 # DELETE FROM telegram_users;
 
 
-INSERT INTO telegram_users (id, username) VALUES (625605,'Lezenford');
-INSERT INTO xmpp_accounts (login, password, server, savehistory, telegramuser) VALUES ('testjava', '12345678', 'ifox.pro',1,625605);
+# INSERT INTO telegram_users (id, username) VALUES (625605,'Lezenford');
+# INSERT INTO xmpp_accounts (login, password, server, savehistory, telegramuser) VALUES ('testjava', '12345678', 'ifox.pro',1,625605);
 # INSERT INTO telegram_chats (chatid, telegramuser, xmppaccount, xmppcontact) VALUES ('-301274558',625605,1,'support812@ifox.pro')
 # INSERT INTO xmpp_accounts (login, password, server) VALUES ('testjava2', '12345678', 'ifox.pro');
